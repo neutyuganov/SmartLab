0c1d2372e9a689de794d8d4a65138939aa8a7fa2
utyuganov <utyuganovsergey55@gmail.com>
2023-06-05 20:59:52 +0600

    Add necessary items

--- app/src/main/java/com/example/smartlab/SplashScreen.java
+++ app/src/main/java/com/example/smartlab/SplashScreen.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGHT = 2000;
+    private final int SPLASH_DISPLAY_LENGHT = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +16,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(SplashScreen.this, OnboardActivity.class);
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
                 SplashScreen.this.startActivity(mainIntent);
                 SplashScreen.this.finish();
             }
